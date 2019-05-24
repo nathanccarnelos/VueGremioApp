25e2c60e083e0ab934110abe39020d7d92e7dd8e
Nathan Carnelos <nathancoltinho@gmail.com>
2019-05-24 20:10:28 -0300

    toolbar no appLayout

--- src/layouts/AppLayout.vue
+++ src/layouts/AppLayout.vue
@@ -1,7 +1,32 @@
 <template>
-  <h1>App</h1>
-</template>
+  <v-app>
+    <v-content>
+      <v-toolbar dark color="primary" dense>
+        <v-toolbar-side-icon></v-toolbar-side-icon>
+        <v-spacer></v-spacer>
 
+        <v-btn icon>
+          <v-icon>search</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>apps</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>refresh</v-icon>
+        </v-btn>
+
+        <v-btn icon>
+          <v-icon>more_vert</v-icon>
+        </v-btn>
+      </v-toolbar>
+      <v-container fluid>
+        <router-view></router-view>
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
 <script>
 export default {
   name: "AppLayout"

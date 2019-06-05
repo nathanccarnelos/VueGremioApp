869ff47161551223536b29cd291b198806d3f978
nathan <nathancoltinho@gmail.com>
2019-06-05 18:15:00 -0300

    d776e7b

---
+++ precache-manifest.9b225d45722ab709b2077da444f726f2.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2675a616b68f446fa6284c111554c7f",
+    "url": "/VueGremioApp/fonts/fa-solid-900.e2675a61.eot"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/VueGremioApp/robots.txt"
+  },
+  {
+    "revision": "f14c3b2ff7c821a4c838debbffd6ad2d",
+    "url": "/VueGremioApp/fonts/fa-solid-900.f14c3b2f.ttf"
+  },
+  {
+    "revision": "7e13e2e1aad0f2e82e03",
+    "url": "/VueGremioApp/css/chunk-vendors.6cefe5e4.css"
+  },
+  {
+    "revision": "d7f10ebcef50b7764e2956c42da7139a",
+    "url": "/VueGremioApp/img/thumb.d7f10ebc.jpg"
+  },
+  {
+    "revision": "7980a6361c25b4665dbbe92d4488783c",
+    "url": "/VueGremioApp/fonts/fa-regular-400.7980a636.woff2"
+  },
+  {
+    "revision": "7aaf5675efd6339e9aba53ecbe5f1e36",
+    "url": "/VueGremioApp/fonts/fa-regular-400.7aaf5675.woff"
+  },
+  {
+    "revision": "859001f6ae8eb0bb3878aaa971b50fc6",
+    "url": "/VueGremioApp/fonts/fa-regular-400.859001f6.eot"
+  },
+  {
+    "revision": "f33342516f7cbe46f1d6b68f9e7bbeda",
+    "url": "/VueGremioApp/fonts/fa-regular-400.f3334251.ttf"
+  },
+  {
+    "revision": "9f4ce3dc689981a1b87faab0f5484f9e",
+    "url": "/VueGremioApp/fonts/fa-brands-400.9f4ce3dc.woff2"
+  },
+  {
+    "revision": "64b3e814a66c2719b15abf8f7998bd73",
+    "url": "/VueGremioApp/fonts/fa-solid-900.64b3e814.woff2"
+  },
+  {
+    "revision": "9f018d1025561e96439f7c0e9026301a",
+    "url": "/VueGremioApp/fonts/fa-brands-400.9f018d10.woff"
+  },
+  {
+    "revision": "0be94a07755ba9b88f2ebcac0f23a3da",
+    "url": "/VueGremioApp/fonts/fa-solid-900.0be94a07.woff"
+  },
+  {
+    "revision": "8e49b728413079dfd9ee45d0c58d54e4",
+    "url": "/VueGremioApp/fonts/fa-brands-400.8e49b728.eot"
+  },
+  {
+    "revision": "b7d071b9c3c197bff4af902070622423",
+    "url": "/VueGremioApp/fonts/fa-brands-400.b7d071b9.ttf"
+  },
+  {
+    "revision": "c51d8f00994ce71955f7",
+    "url": "/VueGremioApp/js/app.ea8359b6.js"
+  },
+  {
+    "revision": "7e13e2e1aad0f2e82e03",
+    "url": "/VueGremioApp/js/chunk-vendors.71dd697b.js"
+  },
+  {
+    "revision": "da8a235bb207c74eea21507f3a86a53b",
+    "url": "/VueGremioApp/img/fa-regular-400.da8a235b.svg"
+  },
+  {
+    "revision": "38975343b586296e9b73e6b56cc3ec5d",
+    "url": "/VueGremioApp/img/fa-brands-400.38975343.svg"
+  },
+  {
+    "revision": "7726a281c1d436eb038f78c6e9048c96",
+    "url": "/VueGremioApp/img/fa-solid-900.7726a281.svg"
+  },
+  {
+    "revision": "ada245517b2c8d09dbdf9bd85b92e74d",
+    "url": "/VueGremioApp/img/bag2.ada24551.jpeg"
+  },
+  {
+    "revision": "fdb0dc4dfd240a643fcebfc2fbbcfbef",
+    "url": "/VueGremioApp/img/bag1.fdb0dc4d.jpeg"
+  },
+  {
+    "revision": "2c7c19ba5f70a85cf9549fd92e04cce5",
+    "url": "/VueGremioApp/img/brasileirao.2c7c19ba.jpeg"
+  },
+  {
+    "revision": "14d7ac321fac3d619739669b89ece36b",
+    "url": "/VueGremioApp/img/camisaFem1.14d7ac32.jpeg"
+  },
+  {
+    "revision": "aecbf29d7f6c77c72747d57f31a20ee6",
+    "url": "/VueGremioApp/img/CamisaMas1.aecbf29d.jpeg"
+  },
+  {
+    "revision": "7892405d05b3063d685e141e63ccc0a7",
+    "url": "/VueGremioApp/img/camisaMas3.7892405d.jpeg"
+  },
+  {
+    "revision": "6405debb20b22e9fb9ee934da1949a1d",
+    "url": "/VueGremioApp/img/bola1.6405debb.jpeg"
+  },
+  {
+    "revision": "0aa0a50e3b919e64a55e8dab73748619",
+    "url": "/VueGremioApp/img/mascote1.0aa0a50e.jpeg"
+  },
+  {
+    "revision": "c133b3e195b8741b6d3c8126422d341b",
+    "url": "/VueGremioApp/img/compra1.c133b3e1.jpg"
+  },
+  {
+    "revision": "b7b3622808ef153e214f5ce7b46d5a4b",
+    "url": "/VueGremioApp/index.html"
+  },
+  {
+    "revision": "c51d8f00994ce71955f7",
+    "url": "/VueGremioApp/css/app.03ed0eb7.css"
+  }
+];
